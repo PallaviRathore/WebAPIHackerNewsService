9f4e89eecb8c9ec1b1bf75cad342ce159c61b7da
PallaviRathore <chauhan.pallavi@gmail.com>
2023-11-17 12:44:00 +0000

    Add project files.

---
+++ WebAPIHackerNewsService/CacheModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPIHackerNewsService
+{
+    public class CacheModel
+    {
+        public IEnumerable<StoryDetails>? Data { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
